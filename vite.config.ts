fd2072f3db54725aa70c3facab9f105bbf9555cc
Severin <nvrsk.severin@gmail.com>
2022-10-27 20:22:25 +0300

    chore: added basic FSD folder structure, aliases, linters etc

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+const ROOT_PATH = path.join(__dirname, './src');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@features': path.resolve(ROOT_PATH, 'features'),
+            '@entities': path.resolve(ROOT_PATH, 'entities'),
+            '@pages': path.resolve(ROOT_PATH, 'pages'),
+            '@ui': path.resolve(ROOT_PATH, 'shared/ui'),
+            '@lib': path.resolve(ROOT_PATH, 'shared/lib'),
+            '@types': path.resolve(ROOT_PATH, 'types'),
+            '@widgets': path.resolve(ROOT_PATH, 'widgets'),
+        },
+    },
+});
